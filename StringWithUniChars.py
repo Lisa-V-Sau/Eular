784f6bebcb1d876beec9d7e6268a0de5f133b6b1
Lisa Sau <lisa.sau@massive-Lisa.local>
2018-02-21 10:13:49 +0000

    Work In Progress

---
+++ StringWithUniChars.py
@@ -0,0 +1,26 @@
+class characters(object):
+
+    def uni_characters(self):
+
+        #def __init__(self):
+        #    self.uni_characters = uni_characters
+
+        qwerty = text1
+
+        text1 = str()
+
+        while text1 is str():
+            if input() == text1:
+                return "not unique"
+
+            else:
+                return "unique"
+
+        print("input")
+        input()
+
+#        if __name__=='__main__':
+#            main()
+        #main()
+
+    uni_characters()
---
+++ StringWithUniCharsDef.py
@@ -0,0 +1,20 @@
+
+def uni_characters(self):
+
+    qwerty = text1
+
+    text1 = str()
+
+    while text1 is str():
+        if input() == text1:
+            if input() == text1:
+                return "not unique"
+
+            else:
+                return "unique"
+
+    print("input")
+    input()
+
+
+uni_characters()
